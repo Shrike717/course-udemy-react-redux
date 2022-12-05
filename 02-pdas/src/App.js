9d193e00f664c53afcd162d6a7accb59005f69ae
Daniel Bauer <dan717@gmx.de>
2022-12-05 09:58:53 +0100

    Done adding props and detructuring

--- 02-pdas/src/App.js
+++ 02-pdas/src/App.js
@@ -6,10 +6,9 @@ function App() {
       <div>
         Personal Digital Assistants
       </div>
-
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
+        <ProfileCard title="Alexa" handle="@alexa99" />
+        <ProfileCard title="Cortana" handle="@cortana32" />
+        <ProfileCard title="Siri" handle="@siri01" />
     </div>
     );
 }
