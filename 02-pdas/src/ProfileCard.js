0f8d033ab85bf0749ec266932240555135316608
Daniel Bauer <dan717@gmx.de>
2022-12-05 18:17:46 +0100

    Done adding jsx

--- 02-pdas/src/ProfileCard.js
+++ 02-pdas/src/ProfileCard.js
@@ -3,11 +3,19 @@ import React from "react";
 function ProfileCard({ title, handle, image}) {
 
   return (
-  <div>
-    <img src={image} alt="pda logo"></img>
-    <div>Title is {title}</div>
-    <div>Handle is {handle}</div>
-  </div>)
-}
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo"></img>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+      </div>
+    </div>)
+  }
 
 export default ProfileCard;
