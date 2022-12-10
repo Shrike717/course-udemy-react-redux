7c02a9ca04144480dbb6ab471144d7432444dd22
Daniel Bauer <dan717@gmx.de>
2022-12-10 09:15:13 +0100

    Comments

--- 04-pics/src/App.js
+++ 04-pics/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import SearchBar from './components/SearchBar';
 
 function App() {
+  // Diese Funktion ist zuständig für die Child-Parent-Kommunikation
+  // Sie bekommt das Suchwort hochgeschickt und löst dann den
+  // API Request aus
   const handleSubmit = (term) => {
     console.log("Do a search with:", term);
   };
+
+  // Hier wird sie mit Props nach unten zur SearchBar gegeben
   return <div>
     <SearchBar onSubmit={handleSubmit} />
     </div>
--- 04-pics/src/components/SearchBar.js
+++ 04-pics/src/components/SearchBar.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { useState } from 'react';
 
+// Hier kommt handleSubmit aus der App.js als Prop an.
+// Hier ist der Name onSubmit willkürlich.
+// Er muss aber gleich sein wiee in App.js
+
 function SearchBar({ onSubmit }) {
+  // Hier wird das PoS gesetzt
   const [term, setTerm] = useState('');
 
+  // Diese Funktion wird gebraucht um das Default-Submit-Verhalten
+  // des Browsers zu unterbrechen.
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
+    // Hier wird handleSubmit aus der App.js aufgerufen und
+    // das Suchwort wieder nach oben geschickt.
     onSubmit({term})
   };
+
+  // Diese Funktion wird gebraucht um React die Kontrolle über das Eingabefeld
+  // zu ermöglichen. Der Suchbegriff wird ausgelesen und dabei
+  // gleichzeitig das PoS damit upgedatet.
 
   const handleChange = (event) => {
     setTerm(event.target.value);
   };
 
+  // Hier wird handleFormSubmit mit Props (Keyword onSubmit) an das form-Tag gedockt
+  // um das Default-Verhalten zu unterbrechen.
+
+  // Am input-Tag wird jede Eingabe abgefangen und updatet damit das PoS
+  // Dann wird mit value die Eingabe ins input-Feld gezwungen.
   return <div>
       <form onSubmit={handleFormSubmit}>
         <input value={term} onChange={handleChange}/>
