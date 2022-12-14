c8daa960112fcf1551db7251a23b536a617ecf69
Daniel Bauer <dan717@gmx.de>
2022-12-14 15:31:37 +0100

    Done toggeling form display

--- 05-books/src/components/BookShow.js
+++ 05-books/src/components/BookShow.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import React from 'react';
+import BookEdit from "./BookEdit";
 
 function BookShow({ book, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
 
-  const handleClick = () => {
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
+  const handleEditClick = () => {
+    // Setzt PoS auf true
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>
+  if (showEdit) {
+    content = <BookEdit />;
+  };
+
   return <div className='book-show'>
-    { book.title }
+   <div>{content}</div>
     <div className='actions'>
-      <button className='delete' onClick={handleClick}>
+      <button className="edit" onClick={handleEditClick}>
+        Edit
+      </button>
+      <button className='delete' onClick={handleDeleteClick}>
         Delete
       </button>
     </div>
