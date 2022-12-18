69e479985cc07a35923dde16e261adbb148085be
Daniel Bauer <dan717@gmx.de>
2022-12-18 15:03:44 +0100

    Done refactoring to use cpntext

--- 05-books/src/components/BookList.js
+++ 05-books/src/components/BookList.js
@@ -4,15 +4,13 @@ import { useContext } from "react";
 import BooksContext from '../context/books';
 
 function BookList({ books, onDelete, onEdit }) {
-  const { count, incrementCount } = useContext(BooksContext);
+
 
   const renderedBooks = books.map((book) => {
     return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>
   })
 
   return <div className='book-list'>
-    {count}
-    <button onClick={incrementCount}>Count</button>
     {renderedBooks}
     </div>;
 }
--- 05-books/src/context/books.js
+++ 05-books/src/context/books.js
@@ -3,17 +3,10 @@ import { createContext, useState } from 'react';
 const BooksContext = createContext();
 
 const Provider = ({ children }) => {
-  const [count, setCount] = useState(5);
 
-  const valueToShare = {
-    count,
-    incrementCount: () => {
-      setCount(count + 1)
-    },
-  };
 
   return (
-  <BooksContext.Provider value={valueToShare}>
+  <BooksContext.Provider value={{}}>
     {children}
   </BooksContext.Provider>)
 };
