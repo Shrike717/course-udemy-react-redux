20aae439e24e883a724ba05c7c33aa7c62570718
Daniel Bauer <dan717@gmx.de>
2023-01-03 18:13:45 +0100

    Done adding icons

--- 06-comps/src/Button.js
+++ 06-comps/src/Button.js
@@ -15,7 +15,7 @@ function Button({
     rounded,
   }
   ) {
-    const classes = className("px-3 py-1.5 border", {
+    const classes = className("flex items-center px-3 py-1.5 border", {
       "border-blue-600 bg-blue-500 text-white": primary,
       "border-gray-900 bg-gray-900 text-white": secondary,
       "border-green-600 bg-green-500 text-white": success,
