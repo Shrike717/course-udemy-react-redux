8c750914d48e42d5707306977a09321d6282eb57
Daniel Bauer <dan717@gmx.de>
2023-01-04 11:50:37 +0100

    Done mapping  over items  and displaying it

--- 06-comps/src/App.js
+++ 06-comps/src/App.js
@@ -4,14 +4,17 @@ import Accordion from "./components/Accordion";
 function App() {
   const items =[
     {
+      id: "jdfr",
       label: "Can i use React on a project?",
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
     },
     {
+      id: "hgcd",
       label: "Can i use JavaScript on a project?",
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
     },
     {
+      id: "nbydc",
       label: "Can i use CSS on a project?",
       content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
     },
--- 06-comps/src/components/Accordion.jsx
+++ 06-comps/src/components/Accordion.jsx
@@ -1,7 +1,15 @@
 const Accordion = ({ items }) =>  {
+  const renderedItems = items.map((item) => {
+    return(
+      <div key={item.id}>
+        <div>{item.label}</div>
+        <div>{item.content}</div>
+      </div>
+    );
+  });
 
   return (
-    <div>Accordion</div>
+    <div>{renderedItems}</div>
   )
 }
 
