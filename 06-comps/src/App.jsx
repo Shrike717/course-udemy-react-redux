c7b58ee7251f96df84e10d8fe04086206ad89685
Daniel Bauer <dan717@gmx.de>
2023-01-05 13:37:01 +0100

    Done creating dropdown component

--- 06-comps/src/App.jsx
+++ 06-comps/src/App.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items =[
-    {
-      id: "jdfr",
-      label: "Can i use React on a project?",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
-    },
-    {
-      id: "hgcd",
-      label: "Can i use JavaScript on a project?",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
-    },
-    {
-      id: "nbydc",
-      label: "Can i use CSS on a project?",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quo distinctio quae est quod aliquid cupiditate odit facilis, omnis facere!",
-    },
-  ]
-  return <Accordion items={items}/>
+  return(
+    <div><Dropdown /></div>
+  )
 };
 
 export default App;
