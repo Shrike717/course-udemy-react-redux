b325a7556c719370b7f84852204d8e505ce53ec3
Daniel Bauer <dan717@gmx.de>
2023-01-06 12:06:47 +0100

    Added styling

--- 06-comps/src/components/Dropdown.jsx
+++ 06-comps/src/components/Dropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,20 +18,25 @@ function Dropdown({ options, value, onChange }) {
     onChange(option);
   })
 
+  const icon = <span className="text-2xl">
+  {isOpen ? <GoChevronDown /> :  <GoChevronLeft />}
+</span>
+
   const renderedOptions = options.map((option) => {
     return(
-      <div onClick={() => {handleOptionClick(option)}} key={option.value}>
-        {isOpen && <div>{option.label}</div>}
+      <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => {handleOptionClick(option)}} key={option.value}>
+        {option.label}
       </div>
     )
   })
 
   return (
-    <div>
-      <div onClick={handleClick}>
+    <div className='w-48 relative'>
+      <div className='flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full' onClick={handleClick}>
         {value?.label || "Selected..."}
+        {icon}
       </div>
-      <div >{renderedOptions}</div>
+      {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}</div>}
     </div>
   )
 }
