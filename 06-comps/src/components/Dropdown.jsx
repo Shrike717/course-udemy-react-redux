7f823da5d15f12c55e53a93628f183860cfe714f
Daniel Bauer <dan717@gmx.de>
2023-01-08 09:21:27 +0100

    Done  using useRef for connection of component to its html

--- 06-comps/src/components/Dropdown.jsx
+++ 06-comps/src/components/Dropdown.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      // console.log(divEl.current);
+    };
+    // Event Listener um Klicks zu ermitteln.
+    document.addEventListener('click', handler, true);
+    // Clear Funktion für Event  Listener
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
   const handleClick = (() => {
     // Setzt bei Klick den Wert des PoS jeweils auf true oder false
@@ -15,7 +29,7 @@ function Dropdown({ options, value, onChange }) {
   const handleOptionClick = ((option) => {
     // Klaoppt Menu wieder ein
     setIsOpen(false);
-    // Schickt Objekt option nach oben zur arent
+    // Schickt Objekt option nach oben zur Parent
     onChange(option);
   })
 
@@ -23,6 +37,7 @@ function Dropdown({ options, value, onChange }) {
   {isOpen ? <GoChevronDown /> :  <GoChevronLeft />}
 </span>
 
+  // Map Funktion rendert die Optionen in der ausgeklappten Liste
   const renderedOptions = options.map((option) => {
     return(
       <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => {handleOptionClick(option)}} key={option.value}>
@@ -32,7 +47,8 @@ function Dropdown({ options, value, onChange }) {
   })
 
   return (
-    <div className='w-48 relative'>
+    // Hier wird devEl an das root Element des Dropdown gesetzt.
+    <div ref={divEl} className='w-48 relative'>
       <Panel
         className='flex justify-between items-center cursor-pointer' onClick={handleClick}
         >
