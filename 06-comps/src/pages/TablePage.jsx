03733bde1787a71573feb812624392b383b02558
Daniel Bauer <dan717@gmx.de>
2023-01-13 13:40:56 +0100

    Done creeating data objects and passing them down

--- 06-comps/src/pages/TablePage.jsx
+++ 06-comps/src/pages/TablePage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import Table from '../components/Table'
+import React from 'react';
+import Table from '../components/Table';
 
 function TablePage() {
-  return (
-    <div><Table /></div>
-  )
-}
+  const data = [
+    {name: "Orange", color: "bg-orange-500", score: 5 },
+    {name: "Apple", color: "bg-red-300", score: 3 },
+    {name: "Banana", color: "bg-yellow-500", score: 1 },
+    {name: "Lime", color: "bg-green-500", score: 4 },
+  ];
 
-export default TablePage
+  return (
+    <div><Table data={data}/></div>
+  )
+};
+
+export default TablePage;
