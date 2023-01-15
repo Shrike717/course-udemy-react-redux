4bc8636884838d12c87a9f9d10c47ab67564dbb8
Daniel Bauer <dan717@gmx.de>
2023-01-15 13:45:52 +0100

    Done creating cells  with nested map  function

--- 06-comps/src/components/Table.jsx
+++ 06-comps/src/components/Table.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 
 function Table({ data, config }) {
+  // Dynamisches Erzeugen der Table Header
   const renderedHeaders = config.map((column) => {
     return(
       <th key={column.label}>{column.label}</th>
     )
   });
-
+  // Dynamisches Erzeugen der Table Rows
   const renderedRows = data.map((fruit) => {
+    // Dynamisches Erzeugen der Cells in den Rows
+    const renderedCells = config.map((column) => {
+      return <td key={column.label} className="p-3">{column.render(fruit)}</td>
+    })
+
     return(
       <tr className='border-b'key={fruit.name}>
-        <td className='p-3'>{config[0].render(fruit)}</td>
-        <td className="p-3">{config[1].render(fruit)}</td>
-        <td className='p-3'>{config[2].render(fruit)}</td>
+        {renderedCells}
       </tr>
     )
   });
