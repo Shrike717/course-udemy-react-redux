49d852c07f77d36ae95c4b5e48207fd661c1abd0
Daniel Bauer <dan717@gmx.de>
2023-01-15 11:52:54 +0100

    Done creating table heeaders dynamically

--- 06-comps/src/pages/TablePage.jsx
+++ 06-comps/src/pages/TablePage.jsx
@@ -3,14 +3,19 @@ import Table from '../components/Table';
 
 function TablePage() {
   const data = [
-    {name: "Orange", color: "bg-orange-500", score: 5 },
-    {name: "Apple", color: "bg-red-300", score: 3 },
-    {name: "Banana", color: "bg-yellow-500", score: 1 },
-    {name: "Lime", color: "bg-green-500", score: 4 },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-300", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
+  const config  = [
+    { label: "Fruits" },
+    { label: "Color" },
+    { label: "Score" },
+  ];
   return (
-    <div><Table data={data}/></div>
+    <div><Table data={data} config={config}/></div>
   )
 };
 
