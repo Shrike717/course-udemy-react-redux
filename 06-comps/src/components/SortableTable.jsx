d1375215e7401d5029fc7a689cf14300eb1e40d3
Daniel Bauer <dan717@gmx.de>
2023-01-17 17:17:50 +0100

    Done implementing sort logic

--- 06-comps/src/components/SortableTable.jsx
+++ 06-comps/src/components/SortableTable.jsx
@@ -6,7 +6,7 @@ function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
 
-  const { config } = props;
+  const { config, data } = props;
 
   const handleClick = (label) => {
     if (sortOrder === null) {
@@ -32,10 +32,31 @@ function SortableTable(props) {
     }
   });
 
+  // Only sort data if sortOrder && sortBy are not null
+  // Make a copy of the "data" prop
+  // Find the correct sortValue function an  use it for sorting
+
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
   return (
     <div>
       {sortOrder} - {sortBy}
-      <Table {...props} config={updatedConfig}/>
+      <Table {...props} data={sortedData} config={updatedConfig}/>
     </div>
   )
 }
