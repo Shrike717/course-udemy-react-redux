f16dcc650560d60cf7f2e24d5b8037a28b55b0ca
Daniel Bauer <dan717@gmx.de>
2023-01-23 11:17:34 +0100

    Done incrementing with reducer

--- 06-comps/src/pages/CounterPage.jsx
+++ 06-comps/src/pages/CounterPage.jsx
@@ -4,7 +4,10 @@ import Button from  "../components/Button";
 import Panel from "../components/Panel";
 
 const reducer = (state, action) => {
-  //
+  return {
+    ...state,
+    count: state.count + 1,
+  }
 };
 
 function CounterPage({ initialCount }) {
@@ -18,7 +21,7 @@ function CounterPage({ initialCount }) {
   console.log(state);
 
   const increment = () => {
-    // setCount(count + 1);
+    dispatch();
   };
 
   const decrement = () => {
