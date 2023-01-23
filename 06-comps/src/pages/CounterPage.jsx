b9a4b00d9b8487b3c8be777d413089af9c5c39b9
Daniel Bauer <dan717@gmx.de>
2023-01-23 16:31:56 +0100

    Done adding switch

--- 06-comps/src/pages/CounterPage.jsx
+++ 06-comps/src/pages/CounterPage.jsx
@@ -7,21 +7,20 @@ const INCREMENT_COUNT = "increment";
 const SET_VALUE_TO_ADD = "set-value-to-add";
 
 const reducer = (state, action) => {
-  if (action.type === INCREMENT_COUNT) {
-    return {
-      ...state,
-      count: state.count + 1,
-    }
-  }
-
-  if (action.type === SET_VALUE_TO_ADD) {
-    return {
-      ...state,
-      valuetoAdd: action.payload,
-    }
-  }
-
-  return state;
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      }
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valuetoAdd: action.payload,
+      }
+    default:
+      return state;
+  };
 };
 
 function CounterPage({ initialCount }) {
