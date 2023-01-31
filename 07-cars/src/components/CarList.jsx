8262b4aadba504c77985f353286b400e69b8fe40
Daniel Bauer <dan717@gmx.de>
2023-01-31 13:07:32 +0100

    Done fixing double keys

--- 07-cars/src/components/CarList.jsx
+++ 07-cars/src/components/CarList.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function CarList() {
+
+  // Get list of cars
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+  console.log(cars);
+
   return (
     <div>CarList</div>
   )
