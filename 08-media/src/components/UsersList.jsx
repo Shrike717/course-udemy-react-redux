829d7268b45050a79f9a892b08ceaa8cc4d48e7c
Daniel Bauer <dan717@gmx.de>
2023-02-05 10:42:49 +0100

    Done adding first thunk to fetch users when app starts the first time

--- 08-media/src/components/UsersList.jsx
+++ 08-media/src/components/UsersList.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
 
 function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return (
     <div>UsersList</div>
   )
