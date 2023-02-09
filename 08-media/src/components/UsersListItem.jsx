b235579ced8f03b573e93bf9d7d013f01e589105
Daniel Bauer <dan717@gmx.de>
2023-02-09 13:02:04 +0100

    Done refactoring components

---
+++ 08-media/src/components/UsersListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UsersListItem({ user }) {
+  return (
+    <div className='mb-2 border rounded'>
+      <div className="p-2 justify-between items-center cursor-pointer">
+        {user.name}
+      </div>
+    </div>
+  )
+}
+
+export default UsersListItem
