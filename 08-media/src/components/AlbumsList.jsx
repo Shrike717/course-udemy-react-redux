177519485adadfe0e867de9cf0d99305760dc373
Daniel Bauer <dan717@gmx.de>
2023-02-12 11:08:20 +0100

    Done creatingg first hook for fetching albums an making use of it

--- 08-media/src/components/AlbumsList.jsx
+++ 08-media/src/components/AlbumsList.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useFetchAlbumsQuery } from '../store'
 
 function AlbumsList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  console.log(data, error,  isLoading);
   return (
     <div>Albums for {user.name}</div>
   )
