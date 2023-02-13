f4e95826d80cbb9a2db67ed0618c69ed0e709b0c
Daniel Bauer <dan717@gmx.de>
2023-02-13 11:30:24 +0100

    Done learning

--- 08-media/src/components/AlbumsList.jsx
+++ 08-media/src/components/AlbumsList.jsx
@@ -7,7 +7,6 @@ import Button from "./Button";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
-  useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
