e750989222a1cf5e19def1d1579c47ecf0876c1c
Daniel Bauer <dan717@gmx.de>
2023-02-15 14:46:15 +0100

    Done adding basic endpoints and tested fetchPhotos

--- 08-media/src/store/index.jsx
+++ 08-media/src/store/index.jsx
@@ -2,15 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return  getDefaultMiddleware()
       .concat(albumsApi.middleware)
+      .concat(photosApi.middleware)
   }
 });
 
@@ -24,3 +27,4 @@ export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
 export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from "./apis/albumsApi";
+export { useFetchPhotosQuery, useAddPhotoMutation, useRemovePhotoMutation } from "./apis/photosApi";
