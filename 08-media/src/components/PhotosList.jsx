d5e4bb23ede836d2f0a40161d0f16e4cc0025dac
Daniel Bauer <dan717@gmx.de>
2023-02-15 16:02:59 +0100

    Done adding photo

--- 08-media/src/components/PhotosList.jsx
+++ 08-media/src/components/PhotosList.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { useFetchPhotosQuery } from '../store'
+import Button from './Button';
+import { useFetchPhotosQuery, useAddPhotoMutation } from '../store'
 
 function PhotosList({ album }) {
   useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddPhotoMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  }
   return (
-    <div>PhotosList</div>
+    <div>
+      <div className='m-2 flex flex-row justify-between items-center'>
+        <h3 className='font-bold'>Photos in Album {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
   )
 }
 
