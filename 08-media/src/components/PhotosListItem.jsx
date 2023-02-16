22cd5a8486a83a4a3c58964e6b41a6ade64f4ef5
Daniel Bauer <dan717@gmx.de>
2023-02-16 10:05:38 +0100

    Done showing list of photos

--- 08-media/src/components/PhotosListItem.jsx
+++ 08-media/src/components/PhotosListItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function PhotosListItem() {
+function PhotosListItem({ photo }) {
   return (
-    <div>PhotosListItem</div>
+    <div>
+      <img className='h-20 w-20' src={photo.url} alt="Random pic"/>
+    </div>
   )
 }
 
