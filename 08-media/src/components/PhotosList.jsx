541b5cd77894414dff20d61425d9f7026524dcab
Daniel Bauer <dan717@gmx.de>
2023-02-16 10:42:23 +0100

    Done adding delete function on mouseover

--- 08-media/src/components/PhotosList.jsx
+++ 08-media/src/components/PhotosList.jsx
@@ -33,7 +33,9 @@ function PhotosList({ album }) {
           + Add Photo
         </Button>
       </div>
-      <div>{content}</div>
+      <div className='mx-8 flex flex-row flex-wrap justify-center'>
+        {content}
+        </div>
     </div>
   )
 }
