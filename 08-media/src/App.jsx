d25cafb4f24ddad541be5bc79de32a616ebd947e
Daniel Bauer <dan717@gmx.de>
2024-06-26 11:47:13 +0200

    08-media: - Added comment how to run the app

--- 08-media/src/App.jsx
+++ 08-media/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import UsersList from './components/UsersList'
+import React from 'react';
+import UsersList from './components/UsersList';
+
+// This app needs to run the json-server: npm run start:server
+// Then run the app: npm start
 
 function App() {
-  return (
-    <div className='container mx-auto'>
-      <UsersList />
-    </div>
-  )
+	return (
+		<div className='container mx-auto'>
+			<UsersList />
+		</div>
+	);
 }
 
-export default App
+export default App;
